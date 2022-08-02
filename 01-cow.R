bc5fb6ff516f80b2e6e8cd1272f70818147fc532
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2022-08-02 13:17:37 +0800

    Update 01-cow.R

--- 01-cow.R
+++ 01-cow.R
@@ -9,46 +9,40 @@ cattle$id <- as.factor(cattle$id)  # convert to factors
 str(cattle)
 write_csv(cattle, file = "cattle.csv")
 
-# Model 1: Null model
-set.seed(456)
-mod1 <- iprior(weight ~ time, cattle, kern = "fbm",
-               control = list(restarts = 4))
+# Model 1: {}
+mod1 <- iprior(weight ~ time, cattle, kernel = "fbm,0.5",
+               est.hurst = TRUE, control = list(restarts = 4))
 
-# Model 2: weight ~ f(time) + f(treatment) + f(time dependent treatment)
-set.seed(456)
-mod2 <- iprior(weight ~ group * time, cattle, kernel = "fbm",
-               method = "em", control = list(restarts = TRUE))
+# Model 2: {X}
+mod2 <- iprior(weight ~ group * time, cattle, kernel = "fbm,0.5",
+               est.hurst = TRUE, control = list(restarts = 4))
 
-# Model 3: weight ~ f(time) + f(cow index) + f(time dependent cow index)
-set.seed(456)
-mod3 <- iprior(weight ~ id * time, cattle, kernel = "fbm",
-               method = "mixed")
+# Model 3: {C}
+mod3 <- iprior(weight ~ id * time, cattle, kernel = "fbm,0.5",
+               est.hurst = TRUE, control = list(restarts = 4))
 
-# Model 4: weight ~ f(time) + f(cow index) +  f(treatment)
-#                   + f(time dependent cow index)
-#                   + f(time dependent treatment)
-set.seed(456)
-mod4 <- iprior(weight ~ group * time +  id * time, cattle,
-               kernel = "fbm", method = "mixed")
+# Model 4: {C,X}
+mod4 <- iprior(weight ~ group * time +  id * time, cattle, kernel = "fbm,0.5",
+               est.hurst = TRUE, control = list(restarts = 4))
 
-# Model 5: weight ~ f(time:cow:treatment)
-set.seed(456)
-mod5 <- iprior(weight ~ id * group * time, cattle, kernel = "fbm",
-               method = "mixed")
+# Model 5: {CX}
+mod5 <- iprior(weight ~ id * group * time, cattle, kernel = "fbm,0.5", 
+               est.hurst = TRUE, control = list(restarts = 4))
 
 # Results table
 cow_table <- function(mod) {
   form <- capture.output(mod$ipriorKernel$formula)
   form <- substring(form, 10)
   tibble(formula = form, loglik = logLik(mod), error = get_prederror(mod),
-         no_lambda = length(coef(mod)) - 1)
+         no_lambda = length(coef(mod)) - 1,
+         hurst = get_hurst(mod))
 }
 tab <- rbind(
   cow_table(mod1), cow_table(mod2), cow_table(mod3),
   cow_table(mod4), cow_table(mod5)
 )
-cbind(model = 1:5, tab)
+cbind(model = 1:5, tab) %>% kbl(format = "rst", digits = 2)
 
-# Plot of fitted regression lines
-plot_fitted_multilevel(mod5, show.legend = FALSE, cred.bands = FALSE) +
-  labs(x = "Time", y = "Weight")
+# # Plot of fitted regression lines
+# plot_fitted_multilevel(mod5, show.legend = FALSE, cred.bands = FALSE) +
+#   labs(x = "Time", y = "Weight")
