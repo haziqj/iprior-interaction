2530c0892af5960952073038d157aef0ea612a02
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2022-08-08 11:24:41 +0800

    updates

---
+++ 02-cow_extended.R
@@ -0,0 +1,79 @@
+# devtools::install_github("haziqj/iprior")
+library(iprior)
+library(tidyverse)
+library(kableExtra)
+
+data(cattle, package = "jmcm")
+names(cattle) <- c("id", "time", "group", "weight")
+cattle$id <- as.factor(cattle$id)  # convert to factors
+# levels(cattle$group) <- c("Treatment A", "Treatment B")
+str(cattle)
+write_csv(cattle, file = "cattle.csv")
+
+mod1 <- kernL(weight ~ group * time, cattle, kernel = "fbm", est.hurst = TRUE,
+              interactions = NULL)
+
+
+
+
+
+iprior_loglik_fn <- function(theta = rnorm(2), modd = mod) {
+  expand.theta <- mod$thetal$theta
+  n.theta <- mod$thetal$n.theta
+  expand.theta[-n.theta] <- theta[1]
+  expand.theta[n.theta] <- theta[2]
+  - 2 * logLik(modd, theta = expand.theta)
+}
+
+
+est_all <- function(kernel = "fbm,0.5", est.hurst = FALSE) {
+  
+  # Model 1: {}
+  mod1 <- iprior(weight ~ time, cattle, kernel = kernel,
+                 est.hurst = est.hurst, control = list(restarts = 0))
+  
+  # Model 2: {X}
+  mod2 <- iprior(weight ~ group * time, cattle, kernel = kernel,
+                 est.hurst = est.hurst, control = list(restarts = 0))
+  
+  # Model 3: {C}
+  mod3 <- iprior(weight ~ id * time, cattle, kernel = kernel,
+                 est.hurst = est.hurst, control = list(restarts = 0))
+  
+  # Model 4: {C,X}
+  mod4 <- iprior(weight ~ group * time +  id * time, cattle, kernel = kernel,
+                 est.hurst = est.hurst, control = list(restarts = 0))
+  
+  # Model 5: {CX}
+  mod5 <- iprior(weight ~ group * id * time, cattle, kernel = kernel, 
+                 est.hurst = est.hurst, control = list(restarts = 0))
+  
+  
+  # Results table
+  cow_table <- function(mod) {
+    form <- capture.output(mod$ipriorKernel$formula)
+    form <- substring(form, 10)
+    tibble(formula = form, loglik = logLik(mod), error = get_prederror(mod),
+           lambda = paste0(round(get_lambda(mod), 3), collapse = ","),
+           psi = iprior::dec_plac(get_psi(mod), 5),
+           hurst = get_hurst(mod))
+  }
+  tab <- rbind(
+    cow_table(mod1), cow_table(mod2), cow_table(mod3),
+    cow_table(mod4), cow_table(mod5)
+  )
+  return(cbind(model = 1:5, tab))
+}
+
+# Hurst = 0.5
+res1 <- est_all(kernel = "fbm,0.5", est.hurst = FALSE)
+
+# Hurst = 0.3
+res2 <- est_all(kernel = "fbm,0.3", est.hurst = FALSE)
+
+# Estimate hurst
+res3 <- est_all(kernel = "fbm", est.hurst = TRUE)
+
+
+
+kbl(res2[c(1,3,5,7,9),], format = "rst", digits = 3)
---
+++ simsv2.R
@@ -0,0 +1,212 @@
+library(glinternet)
+library(glmnet)
+library(tidyverse)
+options(dplyr.summarise.inform = FALSE)
+library(BAS)
+library(doSNOW)
+library(iprior)
+theme_set(theme_classic())
+
+model_codes <- readxl::read_excel("model_codes.xlsx") %>%
+  mutate(code = paste0(x1, x2, x3, x1x2, x1x3, x2x3, x1x2x3, sep = ""))
+
+beta_vals <- readxl::read_excel("model_codes.xlsx", sheet = "beta") 
+  # mutate(code = paste0(as.numeric(x1 > 0), 
+  #                      as.numeric(x2 > 0), 
+  #                      as.numeric(x3 > 0), 
+  #                      as.numeric(x1x2 > 0), 
+  #                      as.numeric(x1x3 > 0), 
+  #                      as.numeric(x2x3 > 0), 
+  #                      as.numeric(x1x2x3 > 0), 
+  #                      sep = ""))
+
+# > model_codes
+# # A tibble: 19 × 8
+# x1    x2    x3  x1x2  x1x3  x2x3 x1x2x3 code   
+# <dbl> <dbl> <dbl> <dbl> <dbl> <dbl>  <dbl> <chr>  
+#   1     1     0     0     0     0     0      0 1000000
+#   2     0     1     0     0     0     0      0 0100000
+#   3     0     0     1     0     0     0      0 0010000
+#   4     1     1     0     1     0     0      0 1101000
+#   5     1     1     0     0     0     0      0 1100000
+#   6     1     0     1     0     1     0      0 1010100
+#   7     1     0     1     0     0     0      0 1010000
+#   8     0     1     1     0     0     1      0 0110010
+#   9     0     1     1     0     0     0      0 0110000
+#  10     1     1     1     1     1     1      1 1111111
+#  11     1     1     1     1     1     1      0 1111110
+#  12     1     1     1     1     1     0      0 1111100
+#  13     1     1     1     1     0     1      0 1111010
+#  14     1     1     1     0     1     1      0 1110110
+#  15     1     1     1     1     0     0      0 1111000
+#  16     1     1     1     0     1     0      0 1110100
+#  17     1     1     1     0     0     1      0 1110010
+#  18     1     1     1     0     0     0      0 1110000
+#  19     0     0     0     0     0     0      0 0000000
+
+the_sim_fn <- function(nsim = 20, n = 200, corr = 0, err.sd = 1.5,
+                       beta.true = c(1, 1, 1, 0.5, 0.5, 0.5, 0.25)) {
+  
+  no.cores <- parallel::detectCores() - 2
+  pb <- txtProgressBar(min = 0, max = nsim, style = 3)
+  progress <- function(i) setTxtProgressBar(pb, i)
+  
+  iprior_est_same_lambda <- function(formulaa = y ~ X1 + X2 + X3, dataa = dat) {
+    mod <- kernL(formulaa, dataa, est.lengthscale = TRUE)
+    # There is a bug in the BlockB function in kernL. Use est.lengthscale = TRUE
+    # to bypass this (unnecessary) function, since we are doing direct
+    # optimisation.
+    iprior_loglik_fn <- function(theta = rnorm(2), modd = mod) {
+      expand.theta <- mod$thetal$theta
+      n.theta <- mod$thetal$n.theta
+      expand.theta[-n.theta] <- theta[1]
+      expand.theta[n.theta] <- theta[2]
+      - 2 * logLik(modd, theta = expand.theta)
+    }
+    
+    res <- NA
+    for (k in 1:1) {
+      optimres <- optim(rnorm(2), iprior_loglik_fn)
+      res[k] <- optimres$value
+    }
+    
+    c(res[which.min(res)], sd(res))
+  }
+  
+  check_lasso <- function(beta) {
+    main.eff <- beta[1:3]
+    twoway.int <- beta[4:6]
+    threeway.int <- beta[7]
+    if (threeway.int > 0) {
+      if (any(main.eff == 0)) return("invalid")
+    }
+    if (twoway.int[1] > 0) {
+      if (main.eff[1] == 0 | main.eff[2] == 0) return("invalid")
+    }
+    if (twoway.int[2] > 0) {
+      if (main.eff[1] == 0 | main.eff[3] == 0) return("invalid")
+    }
+    if (twoway.int[3] > 0) {
+      if (main.eff[2] == 0 | main.eff[3] == 0) return("invalid")
+    }
+    return("valid")
+  }
+  
+  combine_sim_res <- function(z) {
+    final.res <- list()
+    for (k in 1:6) {
+      final.res[[k]] <- data.frame(do.call(rbind, lapply(z, function(x) x[[k]])))
+    }
+    names(final.res) <- names(z[[1]])
+    c(lapply(final.res[1:5], setNames, colnames(final.res$iprior)),
+      lapply(final.res[6], setNames, "sd"))
+  }
+  
+  cl <- makeCluster(no.cores)
+  registerDoSNOW(cl)
+  res <- foreach(
+    i = 1:nsim, #.combine = combine_sim_res, 
+    .packages = c("iprior", "BAS", "glmnet", "rFSA"),
+    .export = c("model_codes"),
+    .options.snow = list(progress = progress)) %dopar% {
+      # Generate data
+      Sigma <- matrix(corr, nrow = 3, ncol = 3)
+      diag(Sigma) <- 1
+      X <- data.frame(mvtnorm::rmvnorm(n, mean = rep(0, 3), sigma = Sigma))
+      X_lasso <- model.matrix(~ 0 + X1 * X2 * X3, X)
+      y <- as.numeric(X_lasso %*% beta.true) + rnorm(n, sd = err.sd)
+      dat <- data.frame(y, X)
+      
+      # LASSO --------------------------------------------------------------------
+      cv_fit <- cv.glmnet(as.matrix(X_lasso), y, nfolds = 10, alpha = 1)
+      beta.lasso <- as.numeric(coef(cv_fit))[-1]
+      res_lasso <- as.numeric(beta.lasso != 0)
+      
+      # t-test -------------------------------------------------------------------
+      mod <- lm(y ~ X1 * X2 * X3, dat)
+      res_lm <- as.numeric(summary(mod)$coefficients[-1, 4] <= 0.1)
+      
+      # I-prior ------------------------------------------------------------------
+      modi <- data.frame(deviance = NA, conv = NA)
+      modi[1,  ]  <- iprior_est_same_lambda(y ~ X1, dat)
+      modi[2,  ]  <- iprior_est_same_lambda(y ~ X2, dat)
+      modi[3,  ]  <- iprior_est_same_lambda(y ~ X3, dat)
+      modi[4,  ]  <- iprior_est_same_lambda(y ~ X1 * X2, dat)
+      modi[5,  ]  <- iprior_est_same_lambda(y ~ X1 + X2, dat)
+      modi[6,  ]  <- iprior_est_same_lambda(y ~ X1 * X3, dat)
+      modi[7,  ]  <- iprior_est_same_lambda(y ~ X1 + X3, dat)
+      modi[8,  ]  <- iprior_est_same_lambda(y ~ X2 * X3, dat)
+      modi[9,  ]  <- iprior_est_same_lambda(y ~ X2 + X3, dat)
+      modi[10, ] <- iprior_est_same_lambda(y ~ X1 * X2 * X3, dat)
+      modi[11, ] <- iprior_est_same_lambda(y ~ X1 * X2 * X3 - X1:X2:X3, dat)
+      modi[12, ] <- iprior_est_same_lambda(y ~ X1 + X2 + X3 + X1:X2 + X1:X3, dat)
+      modi[13, ] <- iprior_est_same_lambda(y ~ X1 + X2 + X3 + X1:X2 + X2:X3, dat)
+      modi[14, ] <- iprior_est_same_lambda(y ~ X1 + X2 + X3 + X1:X3 + X2:X3, dat)
+      modi[15, ] <- iprior_est_same_lambda(y ~ X1 + X2 + X3 + X1:X2, dat)
+      modi[16, ] <- iprior_est_same_lambda(y ~ X1 + X2 + X3 + X1:X3, dat)
+      modi[17, ] <- iprior_est_same_lambda(y ~ X1 + X2 + X3 + X2:X3, dat)
+      modi[18, ] <- iprior_est_same_lambda(y ~ X1 + X2 + X3, dat)
+      bestmod <- which(modi[, 1] == min(modi[, 1], na.rm = TRUE))
+      res_iprior <- unlist(model_codes[bestmod, -8])
+      
+      # g-prior ------------------------------------------------------------------
+      mod <- bas.lm(y ~ X1 * X2 * X3, dat, force.heredity = TRUE,
+                    prior = "EB-local")
+      beta.gprior <- coef(mod, n.models = 1)$postmean[-1]
+      res_gprior <- as.numeric(beta.gprior != 0)
+      
+      # Spike and slab -----------------------------------------------------------
+      mod <- bas.lm(y ~ X1 * X2 * X3, dat, force.heredity = TRUE,
+                    prior = "g-prior", alpha = 100)
+      beta.spikeslab <- coef(mod, n.models = 1)$postmean[-1]
+      res_spikeslab <- as.numeric(beta.spikeslab != 0)
+      
+      list(lasso = res_lasso, lm = res_lm, iprior = res_iprior, 
+           gprior = res_gprior, spikeslab = res_spikeslab, 
+           ipriorconv = modi[bestmod, 2])
+    }
+  
+  close(pb)
+  stopCluster(cl)
+  
+  res <- combine_sim_res(res)
+
+  get_res <- function(x) {
+    x %>%
+      mutate(mod = apply(x, 1, paste0, collapse = "")) %>%
+      rowwise() %>%
+      mutate(no = ifelse(length(which(model_codes$code == mod)) > 0,
+                         which(model_codes$code == mod), NA)) %>%
+      group_by_all() %>%
+      summarise(n = n(), prop = n() / nsim) %>%
+      arrange(desc(n)) %>%
+      ungroup()
+  }
+  
+  the.output <- lapply(res[1:5], get_res)
+  the.output$ipriorconv <- mean(res[6]$ipriorconv[, 1] > 1e-4)
+  the.output$nsim <- nsim
+  the.output$n <- n
+  the.output$corr <- corr
+  the.output$err.sd <- err.sd
+  the.output$beta.true <- beta.true
+  the.output
+}
+
+Nsim <- 10000
+
+myres <- list()
+for (i in seq_len(nrow(beta_vals))) {
+  myres[[i]] <- the_sim_fn(nsim = Nsim, n = 100, corr = 0.5, err.sd = 3,
+                           beta.true = beta_vals[i, ] %>% as.numeric())
+}
+
+save(myres, file = "simres.RData")
+
+myres_uncorr <- list()
+for (i in seq_len(nrow(beta_vals))) {
+  myres_uncorr[[i]] <- the_sim_fn(nsim = Nsim, n = 100, corr = 0, err.sd = 3,
+                                  beta.true = beta_vals[i, ] %>% as.numeric())
+}
+
+save(myres_uncorr, file = "simres_uncorr.RData")
