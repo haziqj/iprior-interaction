c29a20699d7b0b1ce6990acbcc3aabda050867d9
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2022-08-11 10:39:15 +0800

    Update figures

--- 03-sims_analysis.R
+++ 03-sims_analysis.R
@@ -12,8 +12,8 @@ theme_set(theme_classic())
 
 ## ---- simres_fn --------
 res_cleanup <- function(x) {
-  mutate(x, no = ifelse(prop < 0.05, NA, no)) %>%
-    drop_na()
+  # mutate(x, no = ifelse(prop < 0.05, NA, no)) %>%
+  #   drop_na()
   x
 }
 
@@ -61,8 +61,37 @@ res_tab %>%
 res_tab %>%
   pivot_wider(names_from = mod, values_from = prop) %>%
   replace(is.na(.), 0) %>%
-  ggradar()
+  ggradar(group.point.size = 3)
+
+## ---- overall_plot ----
+bind_rows(
+  bind_rows(
+    bind_cols(res_tab1, sim = "Correlated"),
+    bind_cols(res_tab2, sim = "Uncorrelated")
+  ),
+  bind_cols(
+    bind_rows(
+      res_tab1,
+      res_tab2
+    ),
+    sim = "Overall"
+  )
+) %>%
+  pivot_longer(cols = c(iprior, lasso, spikeslab, gprior), names_to = "method",
+               values_to = "prop") %>%
+  group_by(method, sim) %>%
+  summarise(prop = exp(sum(log(prop[prop > 0])) / length(prop))) %>%
+  mutate(method = factor(method, levels = c("iprior", "lasso", "spikeslab", "gprior")),
+         sim = factor(sim, levels = c("Uncorrelated", "Correlated", "Overall"))) -> plot_df
+
+levels(plot_df$method) <- c("I-prior", "Lasso", "Spike & Slab", "g-prior")
 
 
+ggplot(plot_df, aes(method, prop, fill = sim)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_viridis_d() +
+  labs(x = NULL, y = "Proportion correct selection", fill = NULL) -> p
+
+ggsave("simres.pdf", p, width = 8, height = 4)
 
 
